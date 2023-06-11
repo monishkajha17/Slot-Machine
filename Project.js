db5b1c3536afe5cbb8c93643a5e297b0cede61ec
Monishka Jha <jhamonishka@gmail.com>
2023-06-11 17:40:08 +0530

    Updated the project

--- Project.js
+++ Project.js
@@ -90,10 +90,70 @@ const spin = () => {
 
 const transpose = (reels) => {
     const rows = [];
+    for(let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for(let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+};
 
+const printRows = (rows) => {
+    for(const row of rows){
+        let rowString = "";
+        for(const[i,symbol] of row.entries()){
+            rowString += symbol;
+            if(i != row.length - 1){
+                rowString += " | ";
+            }
+        }
+        console.log(rowString);
+    }
+};
+
+const getWinnings = (rows, bet, lines) => {
+    let winnings = 0;
+    for(let row = 0; row < lines; row++){
+        const symbols = rows[row];
+        let allSame = true;
+
+        for(const symbol of symbols){
+            if(symbol != symbols[0]){
+                allSame = false;
+                break;
+            }
+        }
+
+        if(allSame){
+            winnings += bet*SYMBOL_VALUES[symbols[0]];
+        }
+    }
+
+    return winnings;
+};
+
+const game = () => {
+    let balance = deposit();
+
+    while (true){
+        console.log("You have a balance of $", +balance);
+        const NumberOfLines = getNumberOfLines();
+        const bet = getBet(balance, NumberOfLines);
+        balance -= bet * NumberOfLines;
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, NumberOfLines);
+        balance += winnings;
+        console.log("You won, $", +winnings.toString());
+
+        if(balance <=0){
+            console.log("You ran out of money!");
+            break;
+        }
+    }
+    
 }
 
-let balance = deposit();
-const NumberOfLines = getNumberOfLines();
-const bet = getBet(balance, NumberOfLines);
-const reels = spin();
+game();
